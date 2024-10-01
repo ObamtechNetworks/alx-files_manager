8be1a1cf76863791b6b7295298a26b2e524a1eaa
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-10-01 02:03:19 +0100

    used other method to get endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
+const appController = require('../controllers/AppController');
+const { postNew, getMe } = require('../controllers/UsersController');
+const { getConnect, getDisconnect } = require('../controllers/AuthController');
+// const { postUpload, getShow, getIndex } = require('../controllers/FilesController');
+// const { putPublish, putUnpublish, getFile } = require('../controllers/FilesController');
 
 const router = express.Router();
-
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-
-module.exports = router;
+router.get('/status', appController.getStatus); // definition of getStatus
+router.get('/stats', appController.getStats); // definition of getStatus
+router.post('/users', postNew); // definition of postNew
+router.get('/connect', getConnect); // defination of getConnect
+router.get('/disconnect', getDisconnect); // definition of getDisconnect
+router.get('/users/me', getMe);
